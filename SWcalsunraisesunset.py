9c5075c400987679b5e36efe1224f8c1d6524796
Angel Casado <acasadoalonso@gmail.com>
2017-09-15 01:14:17 +0200

    the *funcs now centralized

--- SWcalsunraisesunset.py
+++ SWcalsunraisesunset.py
@@ -1,10 +1,9 @@
-#!/urr/bin/python
+#!/usr/bin/python
 from parserfuncs import *
 import config
 #
-#       get the sunrise/sunset data
+# get the sunrise/sunset data
 #
-
 lat=config.location_latitude
 lon=config.location_longitude
 timeepoc=SRSSgetjsondata(lat, lon, prt=True)
@@ -12,4 +11,4 @@ print timeepoc
 sunsetfile = open (config.DBpath+config.APP+".sunset", 'w') # create a file just to mark that we are alive
 sunsetfile.write(str(timeepoc)+"\n") # write the time as control
 sunsetfile.close()               # close the alive file
-
+exit(0)
