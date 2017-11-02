e49105ceb02bd8b17814651ebe4c29ce167667b0
Angel Casado <acasadoalonso@gmail.com>
2017-11-02 17:19:03 +0100

    sync funcs

--- ss.sh
+++ ss.sh
@@ -1,4 +1,9 @@
-for f in $(ls -ctr /nfs/OGN/srv/*funcs.py ); do
+rm *funcs.py
+rm kglid.py libfap.py
+for f in $(ls -ctr /nfs/OGN/src/*funcs.py ); do
                 echo "Processing file:" $f
-                ln $f .
+                ln -s $f .
 done
+ln -s /nfs/OGN/src/kglid.py .
+ln -s /nfs/OGN/src/libfap.py .
+ls -la *funcs.py
