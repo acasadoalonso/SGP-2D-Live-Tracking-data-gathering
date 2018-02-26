070cf97241fb7834490e67c50bfec35fddc13d4f
Angel Casado <acasadoalonso@gmail.com>
2018-02-26 16:13:11 +0100

    some changes on db structure

--- TRKSTA.sql
+++
@@ -1,3 +0,0 @@
-use APRSLOG;
-DROP   VIEW TRKSTA;
-CREATE VIEW TRKSTA as select *, (select registration from GLIDERS where GLIDERS.idglider = SUBSTRING(OGNTRKSTATUS.id,4,6)) as TRK_NAME from OGNTRKSTATUS;
