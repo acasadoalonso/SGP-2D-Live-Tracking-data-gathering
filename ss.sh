7b56830309394477091987bea3b7ba147665b25b
Angel Casado <acasadoalonso@gmail.com>
2018-05-07 01:04:22 +0200

    OK

--- ss.sh
+++
@@ -1,9 +0,0 @@
-rm *funcs.py
-rm kglid.py libfap.py
-for f in $(ls -ctr /nfs/OGN/src/*funcs.py ); do
-                echo "Processing file:" $f
-                ln -s $f .
-done
-ln -s /nfs/OGN/src/kglid.py .
-ln -s /nfs/OGN/src/libfap.py .
-ls -la *funcs.py
--- ssc.sh
+++
@@ -1,7 +0,0 @@
-rm *funcs.py kglid.py libfap.py
-cp /nfs/OGN/src/*funcs.py .
-cp /nfs/OGN/src/kglid.py .
-cp /nfs/OGN/src/libfap.py .
-git add .
-git commit
-git push origin master
