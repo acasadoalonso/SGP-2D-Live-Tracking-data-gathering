e9673773b36e772d98b2ec6a86a65c12c12638f1
Angel Casado <acasadoalonso@gmail.com>
2018-12-20 18:48:51 +0100

    add inreach documentation

--- flarmfuncs.py
+++ flarmfuncs.py
@@ -1,1 +1,45 @@
-/nfs/OGN/src/flarmfuncs.py
+import kglid
+
+unkglider=[]
+def getflarmid(conn, registration):
+
+	cursG=conn.cursor()             # set the cursor for searching the devices
+	try:
+		cursG.execute("select idglider, flarmtype from GLIDERS where registration = '"+registration+"' ;")
+       	except MySQLdb.Error, e:
+           	try:
+                   	print ">>>MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+              	except IndexError:
+                   	print ">>>MySQL Error: %s" % str(e)
+                     	print ">>>MySQL error:", "select idglider, flarmtype from GLIDERS where registration = '"+registration+"' ;"
+                    	print ">>>MySQL data :",  registration
+		return("NOREG") 
+        rowg = cursG.fetchone() 	# look for that registration on the OGN database
+	if rowg == None:
+		return("NOREG") 
+       	idglider=rowg[0]		# flarmid to report
+       	flarmtype=rowg[1]		# flarmtype flarm/ica/ogn
+	if idglider not in kglid.kglid:	# check that the registration is on the table - sanity check
+		if idglider not in unkglider:
+			print "Warning: flarmid=", idglider, "not on kglid table"
+			unkglider.append(idglider)
+	if flarmtype == 'F':
+		flarmid="FLR"+idglider 	# flarm 
+	elif flarmtype == 'I':
+		flarmid="ICA"+idglider 	# ICA
+	elif flarmtype == 'O':
+		flarmid="OGN"+idglider 	# ogn tracker
+	else: 
+		flarmid="RND"+idglider 	# undefined
+	#print "GGG:", registration, rowg, flarmid
+	return (flarmid)
+# -----------------------------------------------------------
+def chkflarmid(idglider):
+	glider=idglider[3:9]
+	if glider not in kglid.kglid:	# check that the registration is on the table - sanity check
+		if idglider not in unkglider:
+			print "Warning: flarmid=", idglider, "not on kglid table"
+			unkglider.append(idglider)
+			return (False)
+	return (True)
+# -----------------------------------------------------------
