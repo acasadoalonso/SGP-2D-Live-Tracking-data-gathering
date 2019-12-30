037135a676187af989b34da954aaa4487792a701
Angel Casado <acasadoalonso@gmail.com>
2019-12-30 13:12:04 +0100

    delete ss*

--- sscommit.sh
+++
@@ -1,6 +0,0 @@
-rm *funcs.py kglid.py 
-cp /nfs/OGN/src/funcs/*funcs.py .
-cp /nfs/OGN/src/kglid.py .
-git add .
-git commit
-git push origin master
--- sslink.sh
+++
@@ -1,7 +0,0 @@
-rm *funcs.py kglid.py
-ln /nfs/OGN/src/kglid.py .
-for f in $(ls -ctr /nfs/OGN/src/funcs/*funcs.py ); do
-                echo "Processing file:" $f
-                ln -s $f .
-done
-ls -la *funcs.py 
