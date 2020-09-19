de8b33fbfa2b962047af6b2cee9fab5c67c07c1c
Angel Casado <acasadoalonso@gmail.com>
2020-09-19 17:15:19 +0200

    add date and time to ogn.pull.php

--- ogn.pull.php
+++ ogn.pull.php
@@ -4,9 +4,10 @@
 
 if ( $_SERVER['HTTP_X_GITHUB_EVENT'] == 'push') {
         ob_start();
+        passthru("chmod 775 -R * .gi* ");
         passthru("/usr/bin/git --no-pager pull origin master");
         $var = ob_get_contents();
-        echo "RC=".$var."\n";
+        echo "RC=".$var." ".date("Y-m-d H:i:s")."\n";
         passthru("chmod 775 -R * .gi* ");
         passthru("touch UPDATED.by.GIT");
         ob_end_clean(); 
