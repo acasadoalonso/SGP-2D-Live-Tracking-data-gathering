6efe8af6256b9f2f3166a75385e0d2e9c3874d28
Angel Casado <acasadoalonso@gmail.com>
2020-12-05 01:20:35 +0100

    sync

--- doc/adduser.sql
+++ doc/adduser.sql
@@ -1,2 +1,2 @@
-GRANT ALL PRIVILEGES ON *.* TO 'ogn'@'localhost' IDENTIFIED BY 'ogn';
-GRANT select, execute ON *.* TO 'ognread'@'localhost' IDENTIFIED BY 'ognread';
+GRANT ALL PRIVILEGES ON *.* TO 'ogn'@'%' IDENTIFIED BY 'ogn';
+GRANT select, execute ON *.* TO 'ognread'@'%' IDENTIFIED BY 'ognread';
