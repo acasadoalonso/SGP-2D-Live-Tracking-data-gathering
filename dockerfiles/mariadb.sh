fda9c40456472f8e6f61adfafcd341611ead3f44
Angel Casado <acasadoalonso@gmail.com>
2021-01-19 11:33:59 +0100

    Use Docker MariaDB official image instead of mariadb/server

--- dockerfiles/mariadb.sh
+++ dockerfiles/mariadb.sh
@@ -1,3 +1,4 @@
-docker run --net mynetsql --ip 172.18.0.2 --name mariadb -e MYSQL_ROOT_PASSWORD=ogn -d mariadb/server:10.4 --log-bin --binlog-format=MIXED --restart unless-stopped
+#docker run --net mynetsql --ip 172.18.0.2 --name mariadb -e MYSQL_ROOT_PASSWORD=ogn -d mariadb --log-bin --binlog-format=MIXED --restart unless-stopped
+docker run --net mynetsql --ip 172.18.0.2 --name mariadb -e MYSQL_ROOT_PASSWORD=ogn -d mariadb --log-bin --binlog-format=MIXED 
 
 
