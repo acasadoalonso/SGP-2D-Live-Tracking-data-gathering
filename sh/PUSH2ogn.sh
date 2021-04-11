b445a78b85ac52e6b859ae847e08a1de115bde3c
Angel Casado <acasadoalonso@gmail.com>
2021-04-11 18:45:05 +0200

    sync

--- sh/PUSH2ogn.sh
+++ sh/PUSH2ogn.sh
@@ -4,8 +4,8 @@ date                            		>>/tmp/push2ogn.$$.log
 python3 ~/src/APRSsrc/APRScalsunrisesunset.py 	>>/tmp/push2ogn.$$.log
 echo "PUSH2OGN.sh:"            			>>/tmp/push2ogn.$$.log
 echo "===========:"            			>>/tmp/push2ogn.$$.log
-python3 ~/src/APRSsrc/push2ogn.py --SPIDER True --SPOT True --INREACH True --ADSB False		>>push2ogn.$$.log 2>>push2ogn.$$.err &
-#python3 ~/src/APRSsrc/push2ogn.py  --ADSB True --USEDDB True	>>/tmp/push2ogn.$$.log 2>>push2ogn.$$.err &
+python3 ~/src/APRSsrc/push2ogn.py --SPIDER True --SPOT True --INREACH True --ADSB False		>>/tmp/push2ogn.$$.log 2>>/tmp/push2ogn.$$.err &
+#python3 ~/src/APRSsrc/push2ogn.py  --ADSB True --USEDDB True	>>/tmp/push2ogn.$$.log 2>>/tmp/push2ogn.$$.err &
 pgrep -a python3				>>/tmp/push2ogn.$$.log
 cd
 
