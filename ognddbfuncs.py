127dc88b75917615070ffbba689718de2987865a
acasadoalonso <acasado@acm.org>
2025-06-18 02:32:23 +0200

    finally

--- ognddbfuncs.py
+++ ognddbfuncs.py
@@ -95,6 +95,8 @@ def getddbdata(prt=False):                  		# get the data from the API server
     except:
        print("DDB Connecting with: ", DDB_URL, HOST, PORT, " failed ... \n")
        j_obj=''
+    finally:
+       f.close()
     print ("Size of DDB:", len(j_obj))
     return j_obj                            # return the JSON object
 
