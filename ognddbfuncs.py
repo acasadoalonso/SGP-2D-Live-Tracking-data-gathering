e0ecddf614e0054ed9c6b1456c12f819a3d38130
acasadoalonso <acasado@acm.org>
2025-06-18 03:23:25 +0200

    aaaa

--- ognddbfuncs.py
+++ ognddbfuncs.py
@@ -81,7 +81,9 @@ def getddbdata(prt=False):                  		# get the data from the API server
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:101.0) Gecko/20100101 Firefox/101.0'}
     #f = urllib.request.urlopen(req, timeout=10)
     response = requests.get(DDB_URL, headers=headers)
-    print ("RRR2")
+    if response.status_code == 429:
+       time.sleep(int(response.headers["Retry-After"]))
+    print ("RRR2: ", response.status_code)
     try:
        web_page = response.text
        #js=f.read().decode('utf-8')
